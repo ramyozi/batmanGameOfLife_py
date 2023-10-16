8e2416943ad263be0fba33fcee123c0475df7c20
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-16 20:29:04 +0200

    Initial code structure

---
+++ App.py
@@ -0,0 +1,25 @@
+import pygame
+import time
+
+# Dimensions of the grid
+width, height = 80, 30
+cell_size = 20
+
+# Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+# Define button colors
+BUTTON_COLOR = (50, 50, 50)
+BUTTON_HOVER_COLOR = (100, 100, 100)
+
+# Initialize Pygame
+pygame.init()
+
+
+def main():
+    # Game
+
+if __name__ == "__main__":
+    main()
